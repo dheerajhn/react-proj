7ea9f5e0d4204a96971717cd94cae1ee1a31eccc
tousif mudassar <tousifmudassar@gmail.com>
2018-10-01 21:00:25 +0530

    create react folder structure

---
+++ react-project/src/js/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+//expose the persistor to the window so we can flush the local storage through the console...
+
+
+ReactDOM.render(
+  console.log(React,"react" ),
+  document.getElementById("root")
+);
